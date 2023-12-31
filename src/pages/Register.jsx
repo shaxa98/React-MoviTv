21a29d12fa2f936f304f8a830d175cd1d04e5c1d
Shakhzod <howardel@student.21-school.ru>
2023-12-31 22:27:24 +0500

    register updated

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,68 +5,78 @@ const RegisterPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
-
+  const [registered, setRegistered] = useState(false);
+  let users = [];
   const register = () => {
-    console.log({ name: username, password: password, email: email });
+    const user = { name: username, password: password, email: email };
+    users.push(user);
+    setRegistered(true);
+    console.log(users);
   };
 
   return (
     <div className="container">
       <BreadcrumbComponent page="Register" />
-      <div className="logincontainer">
-        <div className="w-25 " id="regisration-form">
-          <h3 className="my-3">Create a Movie-TV account</h3>
-
-          <div className="mb-3">
-            <label htmlFor="username" className="form-label">
-              Username
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="username"
-              onChange={(e) => {
-                setUsername(e.target.value);
-              }}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              aria-describedby="emailHelp"
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            />
-          </div>
-          <button
-            type="submit"
-            className="btn btn-primary btn-lg"
-            id="register-submit"
-            onClick={register}
-          >
-            Submit
-          </button>
+      {registered ? (
+        <div>
+          <h1>Successfully Registered</h1>
         </div>
-      </div>
+      ) : (
+        <div className="logincontainer">
+          <div className="w-25 " id="regisration-form">
+            <h3 className="my-3">Create a Movie-TV account</h3>
+
+            <div className="mb-3">
+              <label htmlFor="username" className="form-label">
+                Username
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="username"
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                aria-describedby="emailHelp"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </div>
+            <button
+              type="submit"
+              className="btn btn-primary btn-lg"
+              id="register-submit"
+              onClick={register}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
